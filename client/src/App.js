4d55ba6841814376e64815f279b7ce23df1de406
shajjad Shagor <shajjadshagor@gmail.com>
2023-01-03 06:20:42 -0800

    all route set and use router new style

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,39 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { Children } from 'react';
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
+import Footer from './components/Footer/Footer';
+import Navbar from './components/Navbar/Navbar';
+import Home from './pages/Home/Home';
+import Product from './pages/Product/Product';
+import Products from './pages/Products/Products';
+
+const Layout = () => {
+  return (
+    <div className='app'>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </div>
+  )
+}
 
 const router = createBrowserRouter([
   {
-    path:"/",
-    element:<span>Home</span>
-  },
-  {
-    path: "/products/:id",
-    element: <span>Category</span>
-  },
-  {
-    path: "/product/:id",
-    element: <span>Product</span>
+    path: "/",
+    element:<Layout />,
+    children: [
+      {
+        path: "/",
+        element:<Home />
+      },
+      {
+        path: "/products/:id",
+        element: <Products />
+      },
+      {
+        path: "/product/:id",
+        element: <Product />
+      },
+    ]
   },
 ]);
 
