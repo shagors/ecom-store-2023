2f7752429198ca8893d5d92aa10d0e0db4cbcdf6
shajjad Shagor <shajjadshagor@gmail.com>
2023-01-03 22:28:04 -0800

    Navbar and footer design

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,8 +1,53 @@
 import React from 'react';
+import './Footer.scss';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='footer'>
+      <div className='top'>
+        <div className='item'>
+          <h1>Categories</h1>
+          <span>Women</span>
+          <span>Men</span>
+          <span>Shoes</span>
+          <span>Accessories</span>
+          <span>New Arrivals</span>
+        </div>
+        <div className='item'>
+          <h1>Links</h1>
+          <span>FAQ</span>
+          <span>Pages</span>
+          <span>Stores</span>
+          <span>Compare</span>
+          <span>Cookies</span>
+        </div>
+        <div className='item'>
+          <h1>About</h1>
+          <span>
+            It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less.
+          </span>
+        </div>
+        <div className='item'>
+          <h1>Contact</h1>
+          <span>
+            It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less.
+          </span>
+        </div>
+      </div>
+      <div className='bottom'>
+        <div className='left'>
+          <span className='logo'>
+            ECOMSTORE
+          </span>
+          <span className='copyright'>
+            &#169; Copyright 2023. All Rights Reserved. 
+          </span>
+        </div>
+        <div className='right'>
+          <img src='/img/payment.png' alt='' />
+        </div>
+      </div>
+    </div>
   )
 }
 
--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,8 +1,63 @@
 import React from 'react';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import SearchIcon from '@mui/icons-material/Search';
+import PersonOutlinedIcon from '@mui/icons-material/PersonOutlined';
+import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+import './Navbar.scss'
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='navbar'>
+        <div className='wrapper'>
+            <div className="left">
+                <div className='item'>
+                    <img src='/img/en.png' alt='' style={{width:"21px", height:"22px"}} />
+                    <KeyboardArrowDownIcon />
+                </div>
+                <div className='item'>
+                    <span>USD</span>
+                    <KeyboardArrowDownIcon />
+                </div>
+                <div className='item'>
+                    <Link className="link" to="/products/1">Women</Link>
+                </div>
+                <div className='item'>
+                    <Link className="link" to="/products/2">Men</Link>
+                </div>
+                <div className='item'>
+                    <Link className="link" to="/products/3">Children</Link>
+                </div>
+            </div>
+            <div className='center'>
+                <Link className="link" to="/">ECOMSTORE</Link>
+            </div>
+            <div className='right'>
+                <div className='item'>
+                    <Link className="link" to="/">Homepage</Link>
+                </div>
+                <div className='item'>
+                    <Link className="link" to="/">About</Link>
+                </div>
+                <div className='item'>
+                    <Link className="link" to="/">Contact</Link>
+                </div>
+                <div className='item'>
+                    <Link className="link" to="/">Store</Link>
+                </div>
+                <div className='icons'>
+                    <SearchIcon />
+                    <PersonOutlinedIcon />
+                    <FavoriteBorderOutlinedIcon />
+                    <div className='cartIcon'>
+                        <ShoppingCartOutlinedIcon />
+                        <span>0</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
