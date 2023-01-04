058a8600b44be874541e34ac305110548881fd37
shajjad Shagor <shajjadshagor@gmail.com>
2023-01-04 05:07:53 -0800

    features container add and image shown in display

--- client/src/components/FeaturedProducts/FeaturedProduct.js
+++ client/src/components/FeaturedProducts/FeaturedProduct.js
@@ -1,9 +1,65 @@
 import React from 'react';
+import Card from '../Card/Card';
 import './FeaturedProduct.scss'
 
-const FeaturedProduct = () => {
+const FeaturedProduct = ({type}) => {
+
+    const data = [
+        {
+            id:1,
+            img:"https://images.pexels.com/photos/1972115/pexels-photo-1972115.jpeg?auto=compress&cs=tinysrgb&w=1600",
+            img2:"https://images.pexels.com/photos/1163194/pexels-photo-1163194.jpeg?auto=compress&cs=tinysrgb&w=1600",
+            title:"",
+            isNew:true,
+            oldPrice:19,
+            price:12,
+        },
+        {
+            id: 2,
+            img: "https://images.pexels.com/photos/1759622/pexels-photo-1759622.jpeg?auto=compress&cs=tinysrgb&w=1600",
+            img2: "",
+            title: "Hat",
+            isNew: true,
+            oldPrice: 19,
+            price: 12,
+        },
+        {
+            id: 3,
+            img: "https://images.pexels.com/photos/1457983/pexels-photo-1457983.jpeg?auto=compress&cs=tinysrgb&w=1600",
+            img2: "",
+            title: "Skirt",
+            isNew: true,
+            oldPrice: 19,
+            price: 12,
+        },
+        {
+            id: 4,
+            img: "https://images.pexels.com/photos/2065200/pexels-photo-2065200.jpeg?auto=compress&cs=tinysrgb&w=1600",
+            img2: "",
+            title: "Skirt",
+            isNew: true,
+            oldPrice: 19,
+            price: 12,
+        },
+    ];
+
   return (
-    <div>FeaturedProduct</div>
+      <div className='featuredProduct'>
+
+        <div className='top'>
+            <h1>{type} products</h1>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in.
+            </p>
+        </div>
+        <div className='bottom'>
+            {
+                data.map(item => (
+                    <Card item={item} key={item.id} />
+                ))
+            }
+        </div>
+      </div>
   )
 }
 
--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import FeaturedProduct from '../../components/FeaturedProducts/FeaturedProduct';
 import Slider from '../../components/Slider/Slider';
 import './Home.scss'
 
@@ -6,6 +7,8 @@ const Home = () => {
   return (
     <div className='home'>
       <Slider />
+      <FeaturedProduct type="featured" />
+      <FeaturedProduct type="trending" />
     </div>
   )
 }
